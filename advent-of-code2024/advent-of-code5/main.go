397a7e42b59dce7151ffac27e5c2166801c79af4
Markus Siegert <spongebobmarkus@gmail.com>
2024-12-05 16:57:24 +0100

    pseudocode to real code

--- advent-of-code2024/advent-of-code5/main.go
+++ advent-of-code2024/advent-of-code5/main.go
@@ -3,84 +3,134 @@ package main
 import (
 	"fmt"
 	"regexp"
-	"structs"
 
-	// "github.com/mitsaucepls/advent-of-code-utils/utils"
+	"github.com/mitsaucepls/advent-of-code-utils/utils"
 	"strings"
 )
 
 func main() {
-	input, err := getAndParseInput()
+	rules, updates, err := getAndParseInput()
 	if err != nil {
 		panic(err)
 	}
+    for _, rule := range rules {
+        fmt.Println(rule)
+    }
+    fmt.Println("Updates:")
+    for _, update := range updates {
+        fmt.Println(update)
+    }
 
-	// for _, row := range input {
-	//     fmt.Println(string(row))
-	// }
+	results := printerRules(rules, updates)
+    fmt.Printf("Result is: %v\n", results)
+    result := 0
+    for _, resultAttr := range results {
+        result += resultAttr
+    }
+    fmt.Println(result)
 
-	result := printerRules(input)
-	fmt.Println(result)
 }
 
-func getAndParseInput() ([]Rule, []int, error) {
-	// input, err := utils.GetInput(5, 2024)
-	// if err != nil {
-	// 	return nil, nil, err
-	// }
-	input := `47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
+func getAndParseInput() ([][]int, [][]int, error) {
+	input, err := utils.GetInput(5, 2024)
+	if err != nil {
+		return nil, nil, err
+	}
+	// input := `47|53
+// 97|13
+// 97|61
+// 97|47
+// 75|29
+// 61|13
+// 75|53
+// 29|13
+// 97|29
+// 53|29
+// 61|53
+// 97|53
+// 61|29
+// 47|13
+// 75|47
+// 97|75
+// 47|61
+// 75|61
+// 47|29
+// 75|13
+// 53|13
 
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47`
+// 75,47,61,53,29
+// 97,61,53,29,13
+// 75,29,13
+// 75,97,47,61,53
+// 61,13,29
+// 97,13,75,29,47`
 
-    rule := regexp.MustCompile(`(\d+)`)
+    // this form of parsing should go to hell
+    re := regexp.MustCompile(`(\d+)`)
     splitStr := strings.Split(input, "\n\n")
-    findgrps(splitStr[0])
-    findgrps(splitStr[1])
-    profit
+    lines0 := strings.Split(splitStr[0], "\n")
+    var rules [][]int
+    for _, line := range lines0 {
+        matches := re.FindAllStringSubmatch(line, -1)
+        var rule []int
+        for _, match := range matches {
+            groups, err := utils.ParseStrSlice2IntSlice(match[1:])
+            if err != nil {
+                panic(err)
+            }
+            rule = append(rule, groups[0])
+        }
+        rules = append(rules, rule)
+    }
+    lines1 := strings.Split(splitStr[1], "\n")
+    var updates [][]int
+    for _, line := range lines1 {
+        if line == "" {
+            continue
+        }
+        matches := re.FindAllStringSubmatch(line, -1)
+        var update []int
+        for _, match := range matches {
+            groups, err := utils.ParseStrSlice2IntSlice(match[1:])
+            if err != nil {
+                panic(err)
+            }
+            update = append(update, groups[0])
+        }
+        updates = append(updates, update)
+    }
 
-	return parsedInput, nil, nil
+	return rules, updates, nil
 }
 
-type Rule struct {
-    big int
-    smoll int
+func printerRules(rules [][]int, updates [][]int) []int {
+    var results []int
+    for _, update := range updates {
+        isCorrect := true
+        fmt.Printf("Update: %v\n", update)
+        for _, rule := range rules {
+            index1 := getIndex(update, rule[0])
+            index2 := getIndex(update, rule[1])
+            if index1 == -1 || index2 == -1 {
+                continue
+            }
+            if !(index1 < index2) {
+                isCorrect = false
+            }
+            fmt.Printf("Rule: %v is: %t with those indicies: %d   %d\n", rule, isCorrect, index1, index2)
+        }
+        if isCorrect {
+            results = append(results, update[len(update) / 2])
+        }
+    }
+    return results
 }
 
-func printerRules() []int {
-    meows = []
-    for update in updates:
-        for rule in rules:
-            getIndex(update, rule[0])
-            getIndex(update, rule[1])
-            if ! index1 > index2
-                break
-
-        meow = update.get(update.len / 2 + 1)
-
-    return meows
+func getIndex(array []int, toSearch int) int {
+    for i, element := range array {
+        if element == toSearch {
+            return i
+        }
+    }
+    return -1
 }
