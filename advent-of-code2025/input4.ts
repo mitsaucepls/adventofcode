abc52f8e4a8164a03a5f20b9a5ff6c71a47a0e63
Markus Siegert <markus.siegert@linux.com>
2025-12-05 12:10:06 +0100

    ci: new day

---
+++ advent-of-code2025/input4.ts
@@ -0,0 +1,107 @@
+import { getInput } from "./utils.ts"
+
+const puzzle_input =  await getInput(4, 2025)
+
+function one(puzzle_input: string): number {
+
+  const rows = puzzle_input.trim().split("\n")
+  let result = 0
+
+  const shelf_map: string[][] = []
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i]
+    shelf_map.push([])
+    for (const roll_slot of row) {
+      shelf_map[i].push(roll_slot)
+    }
+  }
+
+  for (let i = 0; i < shelf_map.length; i++) {
+    for (let j = 0; j < shelf_map[i].length; j++) {
+      if (shelf_map[i][j] === '@') {
+        if (check_adjacent_slots(shelf_map, {y: i, x: j} as Coordinate) < 4) {
+          result++
+        }
+      }
+    }
+  }
+
+
+  return result
+}
+
+function two(puzzle_input: string): number {
+
+  const rows = puzzle_input.trim().split("\n")
+  let result = 0
+
+  const shelf_map: string[][] = []
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i]
+    shelf_map.push([])
+    for (const roll_slot of row) {
+      shelf_map[i].push(roll_slot)
+    }
+  }
+
+  let result_growth = -1
+
+  while (result_growth != 0) {
+    result_growth = 0
+    const tmp_result = result
+    for (let i = 0; i < shelf_map.length; i++) {
+      for (let j = 0; j < shelf_map[i].length; j++) {
+        if (shelf_map[i][j] === '@') {
+          if (check_adjacent_slots(shelf_map, {y: i, x: j} as Coordinate) < 4) {
+            shelf_map[i][j] = '.'
+            result++
+          }
+        }
+      }
+    }
+    result_growth = result - tmp_result
+  }
+
+
+  return result
+}
+
+type Coordinate = {
+  y: number,
+  x: number
+}
+
+const top_left: Coordinate = {y: -1, x: -1}
+const top_top: Coordinate = {y: -1, x: 0}
+const top_right: Coordinate = {y: -1, x: 1}
+const mid_left: Coordinate = {y: 0, x: -1}
+const mid_right: Coordinate = {y: 0, x: 1}
+const bot_left: Coordinate = {y: 1, x: -1}
+const bot_bot: Coordinate = {y: 1, x: 0}
+const bot_right: Coordinate = {y: 1, x: 1}
+
+const adjacent_edges: Coordinate[] = [top_left, top_top, top_right, mid_left, mid_right, bot_left, bot_bot, bot_right]
+
+function check_adjacent_slots(shelf_map: string[][], coordinate: Coordinate): number {
+  let hit: number = 0
+  for (const adjacent_edge of adjacent_edges) {
+    const coordinate_to_check_y = coordinate.y + adjacent_edge.y
+    const coordinate_to_check_x = coordinate.x + adjacent_edge.x
+    if (coordinate_to_check_y < 0 || coordinate_to_check_y >= shelf_map.length) {
+      continue
+    }
+
+    if (coordinate_to_check_x < 0 || coordinate_to_check_x > shelf_map[0].length) {
+      continue
+    }
+
+    if (shelf_map[coordinate_to_check_y][coordinate_to_check_x] === '@') {
+      hit++
+    }
+  }
+
+  return hit
+}
+
+// console.log(one(puzzle_input))
+console.log(two(puzzle_input))
---
+++ advent-of-code2025/input5.ts
@@ -0,0 +1,115 @@
+import { getInput } from "./utils.ts"
+
+const puzzle_input =  await getInput(5, 2025)
+
+type Range = {
+  from: number,
+  to: number
+}
+
+function one(puzzle_input: string): number {
+
+  const rows = puzzle_input.trim().split("\n")
+  let result = 0
+
+  let rangeRowers: Range[] = []
+
+  let secondHalf: boolean = false
+  for (const row of rows) {
+    if (row === "") {
+      secondHalf = true
+    }
+
+    if (!secondHalf) {
+      const range = row.split("-")
+      rangeRowers.push({from: Number(range[0]), to: Number(range[1])})
+    } else {
+      for (const rangeRower of rangeRowers) {
+        if (isInRange(rangeRower, Number(row))) {
+          result++
+          break
+        }
+      }
+    }
+  }
+
+  return result
+}
+
+function two(puzzle_input: string): number {
+
+  const rows = puzzle_input.trim().split("\n")
+
+  let result: number = 0
+  let rangeRowers: Range[] = []
+
+  for (const row of rows) {
+    if (row === "") {
+      break
+    }
+
+    const range = row.split("-")
+    rangeRowers.push({from: Number(range[0]), to: Number(range[1])})
+
+  }
+  // sort by start
+  rangeRowers.sort((a, b) => a.from - b.from)
+
+  let start_from = rangeRowers[0].from
+  let start_to = rangeRowers[0].to
+
+  // we have to seperate overlap and no overlap
+  for (let i = 0; i < rangeRowers.length; i++) {
+    let {from, to} = rangeRowers[i]
+    console.log("start range: { " + start_from + ", " + start_to + " }")
+    console.log("next range: { " + from + ", " + to + " }")
+    const overlaps = overlap({from: start_from, to: start_to}, {from, to})
+    if(overlaps) {
+      // if it overlaps we have to extend the range to the new end
+      start_to = Math.max(start_to, to)
+      console.log("overlaps! New end point is: " + start_to)
+    } else {
+      // otherwise they dont overlap and we can save the range to the result
+      // and set the new starting range
+      const range_to_save = start_to - start_from + 1
+      result += range_to_save
+      start_from = from
+      start_to = to
+      console.log("Does not overlap. Write current range into the result: +" + range_to_save)
+    }
+
+    if (i + 1 === rangeRowers.length && overlaps) {
+      // if the last overlaps save it to the result aswell
+      const range_to_save = start_to - start_from + 1
+      console.log("Write current range into the result: +" + range_to_save)
+      result += range_to_save
+    }
+  }
+
+
+  return result
+}
+
+function overlap(rangeA: Range, rangeB: Range): boolean {
+  // e.g.
+  // 12-18
+  // 16-20
+  // 16 <= 18
+  // This assumes that rangeA and rangeB are sorted by from
+  //
+  // Edge Case:
+  // 10-14
+  // 14-16
+  // 14 <= 14
+
+  return rangeB.from <= rangeA.to
+}
+
+function isInRange(range: Range, rower: number): boolean {
+  return rower >= range.from && rower <= range.to
+}
+
+
+
+// console.log(one(puzzle_input))
+console.log(two(puzzle_input))
